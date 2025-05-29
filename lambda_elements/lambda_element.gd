ad18f928f6c10ca7c351f9611541bbdfd4c71239
mug1wara26 <aloysiusgsq@gmail.com>
2025-05-30 03:18:41 +0800

    playing with drag and drop collisions

--- lambda_elements/lambda_element.gd
+++ lambda_elements/lambda_element.gd
@@ -3,23 +3,36 @@ class_name LambdaElement extends TextureRect
 static var class_string = "LambdaElement"
 @export var source: bool = false
 
+var element_scene = preload("res://lambda_elements/lambda_element.tscn")
+
 func _get_drag_data(position):
   # Create a drag preview Control node (TextureRect, for example)
-  var texture = TextureRect.new()
+  var texture: LambdaElement = element_scene.instantiate()
   texture.texture = self.texture  # Use the same texture
   texture.position = -position
+  texture.enable_collision()
+  
+  if source:
+    texture.source = true
+    
   var drag_preview = Control.new()
   drag_preview.add_child(texture)
   set_drag_preview(drag_preview)
-  if not source:
-    self.hide()
+  
   # Return the drag data as a dictionary (could be expanded later)
   return {
     "type": class_string,
     "offset": -position,
-    "dragged": self
+    "source": self
   }
 
 func _notification(what: int) -> void:
   if (what == NOTIFICATION_DRAG_END):
     self.show()
+
+func enable_collision() ->  void:
+  $Area2D/CollisionShape2D.set_deferred("disabled", false) 
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+  print("area_entered")
