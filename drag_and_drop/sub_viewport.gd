ca803dbec8d352940d4388054bdf7c381055cb2d
notmeegoreng <terencegoh.y.c@gmail.com>
2025-05-30 16:23:35 +0800

    try add bounding hbox

--- drag_and_drop/sub_viewport.gd
+++ drag_and_drop/sub_viewport.gd
@@ -1,17 +1,23 @@
 extends SubViewport
 
+const lambda_application = preload("res://lambda_elements/lambda_application.tscn")
+
 func _process(delta: float) -> void:
   size = get_parent().size
 
-func _on_sub_viewport_container_drop_data(position, data) -> void:
+func _on_sub_viewport_container_drop_data(pos, data) -> void:
   var dragged: LambdaElement = data["source"]
   var camera_position = get_camera_2d().global_position
-  print("drop")
+  var adjusted = pos + data["offset"] + camera_position - Vector2(get_viewport().size/2)
   if dragged.source:
-    # not 100% if right flag
+    # right now, this should always be true
     dragged = dragged.duplicate(DuplicateFlags.DUPLICATE_USE_INSTANTIATION)
-    print("dupe")
     dragged.source = false
-    add_child(dragged)
-  dragged.position = position + data["offset"] + camera_position - Vector2(get_viewport().size/2)
+    # check if added to existing hbox
+    # otherwise:
+    var application = lambda_application.instantiate()
+    application.position = adjusted
+    dragged.position = Vector2.ZERO 
+    application.add_child(dragged)
+    add_child(application)
   dragged.enable_collision()
